233d12dcf8142a2d929f281da7e681e85a27b052
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-05-13 10:31:33 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
-import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+// import { FocusScreen } from "./components/04-useRef/FocusScreen";
+import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./components/02-useEffect/SimpleForm";
@@ -9,7 +11,7 @@ import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHook
 
 ReactDOM.render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <RealExampleRef />
   </React.StrictMode>,
   document.getElementById("root")
 );
